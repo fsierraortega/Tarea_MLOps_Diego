5e4297347632849888dcb045d850b09ecf613ebe
fsierraortega <90199032+fsierraortega@users.noreply.github.com>
2024-10-25 10:00:24 -0300

    Primera carga

--- app.py
+++ app.py
@@ -50,5 +50,9 @@ async def model_predict(Major_Axis_Length: float,
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+
 if __name__ == "__main__":
-    uvicorn.run(app, port=8080, host="0.0.0.0")
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
